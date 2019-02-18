48119ccc4e291e2c442def2b017580a24688c825
estudiante <estudiante@ISIS2603-500.sis.virtual.uniandes.edu.co>
2019-02-18 15:26:22 -0500

    BlogDTO

--- s2_idiomas-api/src/main/java/co/edu/uniandes/csw/idiomas/dtos/BlogDTO.java
+++ s2_idiomas-api/src/main/java/co/edu/uniandes/csw/idiomas/dtos/BlogDTO.java
@@ -5,10 +5,87 @@
  */
 package co.edu.uniandes.csw.idiomas.dtos;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  *
  * @author estudiante
  */
 public class BlogDTO {
+    String texto;
+    String imagen;
+    String video;
+    Long grupoDeInteres;
     
+    public BlogDTO(){
+        
+    }
+        /**
+     * Devuelve el Idioma del grupo.
+     *
+     * @return the id
+     */
+    public String getTexto() {
+        return texto;
+    }
+        
+       /**
+     * Devuelve el Idioma del grupo.
+     *
+     * @return the id
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * Modifica el Admin dl grupo.
+     *
+     * @param admin the admin to set
+     */
+    public void setTexto (String pTexto) {
+        this.texto = pTexto;
+    }
+ 
+    public String getVideo() {
+        return video;
+    }
+
+    /**
+     * Modifica la califiacion del grupo.
+     *
+     * @param calificacion the calificacion to set
+     */
+    public void setImagen(String pImagen) {
+        this.imagen = pImagen;
+    }
+       /**
+     * Modifica la califiacion del grupo.
+     *
+     * @param calificacion the calificacion to set
+     */
+    public void setVideo(String pVideo) {
+        this.video = pVideo;
+    }
+      /**
+     * retorna la califiacion del grupo.
+     *
+     */
+    public Long getGrupoDeInteres() {
+       return grupoDeInteres;
+    }
+    
+    public void setGrupoDeInteres(Long pGrupo) {
+        this.grupoDeInteres = pGrupo;
+    }
+
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+}
+
+
+
 }
