c5106220a99d3e50416f27e0c60a0bf88f888761
jdruedaa <jd.ruedaa@uniandes.edu.co>
2019-03-08 23:49:52 -0500

    test persistence calificacion

---
+++ s2_idiomas-back/src/test/java/co/edu/uniandes/csw/idiomas/test/persistence/CalificacionPersistenceTest.java
@@ -0,0 +1,198 @@
+package co.edu.uniandes.csw.idiomas.test.persistence;
+
+import co.edu.uniandes.csw.idiomas.entities.ActividadEntity;
+import co.edu.uniandes.csw.idiomas.entities.CalificacionEntity;
+import co.edu.uniandes.csw.idiomas.persistence.CalificacionPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.junit.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ * Pruebas de persistencia de Actividad
+ *
+ * @author g.cubillosb
+ */
+@RunWith(Arquillian.class)
+public class CalificacionPersistenceTest {
+    
+    // ------------------------------------------------------------------------
+    // Atributos
+    // ------------------------------------------------------------------------
+
+    /**
+     * Inyecta la dependencia de ActividadPersistence.
+     */
+    @Inject
+    private CalificacionPersistence calificacionPersistence;
+
+    /**
+     * Contexto de persistencia que se va a utilizar para acceder a la base
+     * de datos.
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Variable para marcar las transacciones del "em" (Entity Manager) cuando
+     * se crean/borran datos.
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * Lista de los datos de prueba.
+     */
+    private List<CalificacionEntity> data = new ArrayList<>();
+    
+    // ------------------------------------------------------------------------
+    // Métodos
+    // ------------------------------------------------------------------------
+
+    /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(CalificacionEntity.class.getPackage())
+                .addPackage(CalificacionPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from CalificacionEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            CalificacionEntity entity = factory.manufacturePojo(CalificacionEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
+    /**
+     * Prueba para crear un Actividad.
+     */
+    @Test
+    public void createCalificacionTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        CalificacionEntity newEntity = factory.manufacturePojo(CalificacionEntity.class);
+        CalificacionEntity result = calificacionPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        ActividadEntity entity = em.find(ActividadEntity.class, result.getId());
+
+        Assert.assertTrue(Objects.equals(newEntity.getCalificacion(), entity.getCalificacion()));
+    }
+
+    /**
+     * Prueba para consultar la lista de Actividades.
+     */
+    @Test
+    public void getCalificacionesTest() {
+        List<CalificacionEntity> list = calificacionPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (CalificacionEntity ent : list) {
+            boolean found = false;
+            for (CalificacionEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+    /**
+     * Prueba para consultar un Actividad.
+     */
+    @Test
+    public void getCalificacionTest() {
+        CalificacionEntity entity = data.get(0);
+        CalificacionEntity newEntity = calificacionPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertTrue(Objects.equals(entity.getCalificacion(), newEntity.getCalificacion()));
+        Assert.assertEquals(entity.getMensaje(), newEntity.getMensaje());
+    }
+
+    /**
+     * Prueba para actualizar un Actividad.
+     */
+    @Test
+    public void updateCalificacionTest() {
+        CalificacionEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        CalificacionEntity newEntity = factory.manufacturePojo(CalificacionEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        calificacionPersistence.update(newEntity);
+
+        CalificacionEntity resp = em.find(CalificacionEntity.class, entity.getId());
+        
+        Assert.assertEquals(resp.getId(), newEntity.getId());
+        Assert.assertTrue(Objects.equals(entity.getCalificacion(), newEntity.getCalificacion()));
+        Assert.assertEquals(resp.getMensaje(), newEntity.getMensaje());
+    }
+
+    /**
+     * Prueba para eliminar un Actividad.
+     */
+    @Test
+    public void deleteCalificacionTest() {
+        CalificacionEntity entity = data.get(0);
+        calificacionPersistence.delete(entity.getId());
+        CalificacionEntity deleted = em.find(CalificacionEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+}
+
+
