17b94b83579645e54b31ddf495d68281aed3904f
j.barbosaj <j.barbosaj@uniandes.edu.co>
2019-04-28 21:52:09 -0500

    corregi un error

--- s2_idiomas-api/src/main/java/co/edu/uniandes/csw/idiomas/resources/AnfitrionResource.java
+++ s2_idiomas-api/src/main/java/co/edu/uniandes/csw/idiomas/resources/AnfitrionResource.java
@@ -7,9 +7,9 @@ package co.edu.uniandes.csw.idiomas.resources;
 
 
 
-import co.edu.uniandes.csw.idiomas.dtos.AdministradorDTO;
-import co.edu.uniandes.csw.idiomas.ejb.AdministradorLogic;
-import co.edu.uniandes.csw.idiomas.entities.AdministradorEntity;
+import co.edu.uniandes.csw.idiomas.dtos.AnfitrionDTO;
+import co.edu.uniandes.csw.idiomas.ejb.AnfitrionLogic;
+import co.edu.uniandes.csw.idiomas.entities.AnfitrionEntity;
 import co.edu.uniandes.csw.idiomas.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.idiomas.mappers.BusinessLogicExceptionMapper;
 import co.edu.uniandes.csw.idiomas.mappers.WebApplicationExceptionMapper;
@@ -46,122 +46,122 @@ public class AnfitrionResource
     private static final String NO_EXISTE =" no existe." ;
 
     @Inject
-    AdministradorLogic administradorLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+    AnfitrionLogic anfitrionLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
 
     /**
-     * Crea una nueva administrador con la informacion que se recibe en el cuerpo de
+     * Crea una nueva anfitrion con la informacion que se recibe en el cuerpo de
      * la petición y se regresa un objeto identico con un id auto-generado por
      * la base de datos.
      *
-     * @param administrador {@link AdministradorDTO} - La administrador que se desea
+     * @param anfitrion {@link AnfitrionDTO} - La anfitrion que se desea
      * guardar.
-     * @return JSON {@link AdministradorDTO} - La administrador guardada con el atributo
+     * @return JSON {@link AnfitrionDTO} - La anfitrion guardada con el atributo
      * id autogenerado.
      * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} -
-     * Error de lógica que se genera cuando ya existe la administrador.
+     * Error de lógica que se genera cuando ya existe la anfitrion.
      */
     @POST
-    public AdministradorDTO createAdministrador(AdministradorDTO administrador) throws BusinessLogicException {
-       LOGGER.log(Level.INFO, "AdministradorResource createAdministrador: input: {0}", administrador);
+    public AnfitrionDTO createAnfitrion(AnfitrionDTO anfitrion) throws BusinessLogicException {
+       LOGGER.log(Level.INFO, "AnfitrionResource createAnfitrion: input: {0}", anfitrion);
         // Convierte el DTO (json) en un objeto Entity para ser manejado por la lógica.
-        AdministradorEntity administradorEntity = administrador.toEntity();
-        // Invoca la lógica para crear la administrador nueva
-        AdministradorEntity nuevoAdministradorEntity = administradorLogic.createAdministrador(administradorEntity);
+        AnfitrionEntity anfitrionEntity = anfitrion.toEntity();
+        // Invoca la lógica para crear la anfitrion nueva
+        AnfitrionEntity nuevoAnfitrionEntity = anfitrionLogic.createAnfitrion(anfitrionEntity);
         // Como debe retornar un DTO (json) se invoca el constructor del DTO con argumento el entity nuevo
-        AdministradorDTO nuevoAdministradorDTO = new AdministradorDTO(nuevoAdministradorEntity);
-        LOGGER.log(Level.INFO, "AdministradorResource createAdministrador: output: {0}", nuevoAdministradorDTO);
-        return nuevoAdministradorDTO;
+        AnfitrionDTO nuevoAnfitrionDTO = new AnfitrionDTO(nuevoAnfitrionEntity);
+        LOGGER.log(Level.INFO, "AnfitrionResource createAnfitrion: output: {0}", nuevoAnfitrionDTO);
+        return nuevoAnfitrionDTO;
     }
 
     /**
-     * Borra la administrador con el id asociado recibido en la URL.
+     * Borra la anfitrion con el id asociado recibido en la URL.
      *
-     * @param administradoresId Identificador de la administrador que se desea borrar.
+     * @param anfitrionesId Identificador de la anfitrion que se desea borrar.
      * Este debe ser una cadena de dígitos.
      * @return 
      */
     @DELETE
-    @Path("{administradoresId: \\d+}")
-    public void deleteAdministrador(@PathParam("administradoresId") Long administradoresId) throws BusinessLogicException {
-        LOGGER.log(Level.INFO, "AdministradorResource deleteAdministrador: input: {0}", administradoresId);
-        if (administradorLogic.getAdministrador(administradoresId) == null) {
-            throw new WebApplicationException("El recurso /administradores/" + administradoresId + NO_EXISTE , 404);
+    @Path("{anfitrionesId: \\d+}")
+    public void deleteAnfitrion(@PathParam("anfitrionesId") Long anfitrionesId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "AnfitrionResource deleteAnfitrion: input: {0}", anfitrionesId);
+        if (anfitrionLogic.getAnfitrion(anfitrionesId) == null) {
+            throw new WebApplicationException("El recurso /anfitriones/" + anfitrionesId + NO_EXISTE , 404);
         }
-        administradorLogic.deleteAdministrador(administradoresId);
-        LOGGER.info("AdministradorResource deleteAdministrador: output: void");
+        anfitrionLogic.deleteAnfitrion(anfitrionesId);
+        LOGGER.info("AnfitrionResource deleteAnfitrion: output: void");
     }
     
    /**
      * Actualiza el autor con el id recibido en la URL con la información que se
      * recibe en el cuerpo de la petición.
      *
-     * @param AdministradorId Identificador del autor que se desea actualizar. Este
+     * @param AnfitrionId Identificador del autor que se desea actualizar. Este
      * debe ser una cadena de dígitos.
-     * @param Administrador {@link AdministradorDTO} El autor que se desea guardar.
-     * @return JSON {@link AdministradorDTO} - El autor guardado.
+     * @param Anfitrion {@link AnfitrionDTO} El autor que se desea guardar.
+     * @return JSON {@link AnfitrionDTO} - El autor guardado.
      * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
      * Error de lógica que se genera cuando no se encuentra el autor a
      * actualizar.
      */
     @PUT
-    @Path("{AdministradorId: \\d+}")
-    public AdministradorDTO updateAdministrador(@PathParam("AdministradorId") Long administradorId, AdministradorDTO administrador) throws BusinessLogicException {
-        LOGGER.log(Level.INFO, "AdministradorResource updateAdministrador: input: AdministradorId: {0} , Administrador: {1}", new Object[]{administradorId, administrador});
-        administrador.setId(administradorId);
-        if (administradorLogic.getAdministrador(administradorId) == null)
+    @Path("{AnfitrionId: \\d+}")
+    public AnfitrionDTO updateAnfitrion(@PathParam("AnfitrionId") Long anfitrionId, AnfitrionDTO anfitrion) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "AnfitrionResource updateAnfitrion: input: AnfitrionId: {0} , Anfitrion: {1}", new Object[]{anfitrionId, anfitrion});
+        anfitrion.setId(anfitrionId);
+        if (anfitrionLogic.getAnfitrion(anfitrionId) == null)
         {
-            throw new WebApplicationException("El recurso /Administradores/" + administradorId +NO_EXISTE, 404);
+            throw new WebApplicationException("El recurso /Anfitriones/" + anfitrionId +NO_EXISTE, 404);
         }
-        AdministradorDTO detailDTO = new AdministradorDTO(administradorLogic.updateAdministrador(administradorId, administrador.toEntity()));
-        LOGGER.log(Level.INFO, "AdministradorResource updateAdministrador: output: {0}", detailDTO);
+        AnfitrionDTO detailDTO = new AnfitrionDTO(anfitrionLogic.updateAnfitrion(anfitrionId, anfitrion.toEntity()));
+        LOGGER.log(Level.INFO, "AnfitrionResource updateAnfitrion: output: {0}", detailDTO);
         return detailDTO;
     }
     
      /**
-     * Actualiza la administrador con el id asociado recibido en la URL.
+     * Actualiza la anfitrion con el id asociado recibido en la URL.
      *
-     * @param administradoresId Identificador de la administrador que se desea actualizar.
+     * @param anfitrionesId Identificador de la anfitrion que se desea actualizar.
      * Este debe ser una cadena de dígitos.
      * @return 
      */
     @GET
-    @Path("{administradoresId: \\d+}")
-    public AdministradorDTO retornarAdministrador(@PathParam("administradoresId") Long administradoresId) {
-        LOGGER.log(Level.INFO, "AdministradorResource getAdministrador: input: {0}", administradoresId);
-        AdministradorEntity administradorEntity = administradorLogic.getAdministrador(administradoresId);
-        if (administradorEntity == null) {
-            throw new WebApplicationException("El recurso /administradores/" + administradoresId +NO_EXISTE, 404);
+    @Path("{anfitrionesId: \\d+}")
+    public AnfitrionDTO retornarAnfitrion(@PathParam("anfitrionesId") Long anfitrionesId) {
+        LOGGER.log(Level.INFO, "AnfitrionResource getAnfitrion: input: {0}", anfitrionesId);
+        AnfitrionEntity anfitrionEntity = anfitrionLogic.getAnfitrion(anfitrionesId);
+        if (anfitrionEntity == null) {
+            throw new WebApplicationException("El recurso /anfitriones/" + anfitrionesId +NO_EXISTE, 404);
         }
-        AdministradorDTO detailDTO = new AdministradorDTO(administradorEntity);
-        LOGGER.log(Level.INFO, "AdministradorResource getAdministrador: output: {0}", detailDTO);
+        AnfitrionDTO detailDTO = new AnfitrionDTO(anfitrionEntity);
+        LOGGER.log(Level.INFO, "AnfitrionResource getAnfitrion: output: {0}", detailDTO);
         return detailDTO;
     }
     
       /**
-     * Actualiza la administrador con el id asociado recibido en la URL.
+     * Actualiza la anfitrion con el id asociado recibido en la URL.
      *
-     * @param administradoresId Identificador de la administrador que se desea actualizar.
+     * @param anfitrionesId Identificador de la anfitrion que se desea actualizar.
      * Este debe ser una cadena de dígitos.
      * @return 
      */
     @GET    
-    public List<AdministradorDTO> retornarAdministrador() {
-        LOGGER.info("AdministradorResource getAdministradors: input: void");
-        List<AdministradorDTO> listaAdministradors = listEntity2DTO(administradorLogic.getAdministradors());
-        LOGGER.log(Level.INFO, "AdministradorResource getAdministradors: output: {0}", listaAdministradors);
-        return listaAdministradors;
+    public List<AnfitrionDTO> retornarAnfitrion() {
+        LOGGER.info("AnfitrionResource getAnfitrions: input: void");
+        List<AnfitrionDTO> listaAnfitrions = listEntity2DTO(anfitrionLogic.getAnfitrions());
+        LOGGER.log(Level.INFO, "AnfitrionResource getAnfitrions: output: {0}", listaAnfitrions);
+        return listaAnfitrions;
                
     }   
       /**
-     * Convierte una lista de AdministradorEntity a una lista de AdministradorDTO.
+     * Convierte una lista de AnfitrionEntity a una lista de AnfitrionDTO.
      *
-     * @param entityList Lista de AdministradorEntity a convertir.
-     * @return Lista de AdministradorDTO convertida.
+     * @param entityList Lista de AnfitrionEntity a convertir.
+     * @return Lista de AnfitrionDTO convertida.
      */
-    private List<AdministradorDTO> listEntity2DTO(List<AdministradorEntity> entityList) {
-        List<AdministradorDTO> list = new ArrayList<>();
-        for (AdministradorEntity entity : entityList) {
-            list.add(new AdministradorDTO(entity));
+    private List<AnfitrionDTO> listEntity2DTO(List<AnfitrionEntity> entityList) {
+        List<AnfitrionDTO> list = new ArrayList<>();
+        for (AnfitrionEntity entity : entityList) {
+            list.add(new AnfitrionDTO(entity));
         }
         return list;
     }
